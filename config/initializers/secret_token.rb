679a8ba14fd374eaa7f7baaf688eecee0815397c
Moshik <mraccah@agilian.com>
2011-02-18 14:47:54 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewRailsNoDB::Application.config.secret_token = 'cfe5a02e9bd2e5df58b25523ff4d2fae3e407db61d16687bf39e4bcfc590aea70a38e88a0d516599a6b119bf39a65ef6135811b2293eea48c6e77855e98371d6'
